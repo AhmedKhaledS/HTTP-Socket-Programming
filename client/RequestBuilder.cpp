08ca01a0a9eb4b2885befded84dddf75ad65b14d
ahmednaggar19 <ahmednagar2011@gmail.com>
2018-11-17 15:24:12 +0200

    added initial implementation for sending and receiving

---
+++ client/RequestBuilder.cpp
@@ -0,0 +1,40 @@
+//
+// Created by ahmednagga19 on 17/11/18.
+//
+
+#include "RequestBuilder.h"
+#include "../fileServices/FileReader.h"
+
+
+#define LINE_END "\r\n"
+#define VERSION "HTTP/1.1"
+
+using namespace std;
+
+const string HEADERS = "Host: www.tutorialspoint.com\r\n"
+                       "User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\r\n"
+                       "Accept: text/html,application/xhtml+xml\r\n"
+                       "Accept-Language: en-us\r\n"
+                       "Accept-Encoding: gzip, deflate\r\n"
+                       "Accept-Charset: ISO-8859-1,utf-8;q=0.7\r\n"
+                       "Connection: Keep-Alive\r\n"
+                       "\r\n";
+
+RequestBuilder::RequestBuilder() {
+
+}
+
+std::string RequestBuilder::build_request_message(RequestCommand command) {
+    string request = "";
+    request += (command.getType() == REQ_TYPE::GET ? "GET " : "POST ");
+    request += (command.getFile_name());
+    request += (" ");
+    request += VERSION;
+    request += LINE_END;
+    request += HEADERS;
+    if (command.getType() == REQ_TYPE::POST) {
+        FileReader* reader = new FileReader();
+        request += reader->read_file(command.getFile_name());
+    }
+    return request;
+}
